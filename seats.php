c1cfd3272d5315ec6adebe95e40af8f78cce8ed9
mohammed chamma <hms.mohammed@gmail.com>
2014-02-01 00:28:52 -0500

    put a 'coming soon' on the seats page

--- seats.php
+++ seats.php
@@ -42,70 +42,72 @@ while ($p = $res->fetch_assoc()) {
     }
 }
 
-$src = '';
+$src = '<h2>Coming soon!</h2><br> We\'ve been forced to change venues at the last minute and need to make a new seating layout.';
 
-// Display instructions and legend
-// Note: The display is different if the user is not logged in.
-if (isSet($_p)) {
-    $seat_str = '';
-    if ($_p['seat']) {
-        $seat_str = sPrintF(', seat %1$s', $_p['seat']);
-    }
-    $src .= sPrintF('
-<fieldset class="faded-bg" style="float:right;margin-left:1em;width:380px;">
-<legend>Seat Legend</legend>
-<table class="center seating-chart">
-<col /><col /><col width="25" />
-<col /><col /><col width="25" />
-<col /><col />
-<tr>
-<th class="vac"><input type="radio" /></th><td>&nbsp; Available</td><td></td>
-<th class="occ"><input type="checkbox" /></th><td>&nbsp; Taken</td><td></td>
-<th class="me"><input type="checkbox" /></th><td>&nbsp; You%1$s</td>
-</tr>
-</table>
-</fieldset>
+// $src = '';
 
-<h1>Seating Plan</h1>
-<p>Choose your desired seat on the map below.  Your seat number will be used to determine which network port and power outlet you should use to setup your computer.</p>
-<p>You may hover over any seat with your cursor to see its detailed status.</p>
+// // Display instructions and legend
+// // Note: The display is different if the user is not logged in.
+// if (isSet($_p)) {
+//     $seat_str = '';
+//     if ($_p['seat']) {
+//         $seat_str = sPrintF(', seat %1$s', $_p['seat']);
+//     }
+//     $src .= sPrintF('
+// <fieldset class="faded-bg" style="float:right;margin-left:1em;width:380px;">
+// <legend>Seat Legend</legend>
+// <table class="center seating-chart">
+// <col /><col /><col width="25" />
+// <col /><col /><col width="25" />
+// <col /><col />
+// <tr>
+// <th class="vac"><input type="radio" /></th><td>&nbsp; Available</td><td></td>
+// <th class="occ"><input type="checkbox" /></th><td>&nbsp; Taken</td><td></td>
+// <th class="me"><input type="checkbox" /></th><td>&nbsp; You%1$s</td>
+// </tr>
+// </table>
+// </fieldset>
 
-<form action="#" onsubmit="return chooseSeat(this);">
-', $seat_str);
+// <h1>Seating Plan</h1>
+// <p>Choose your desired seat on the map below.  Your seat number will be used to determine which network port and power outlet you should use to setup your computer.</p>
+// <p>You may hover over any seat with your cursor to see its detailed status.</p>
 
-} else {
-    $src .= '
-<fieldset class="faded-bg" style="float:right;margin-left:1em;width:380px;">
-<legend>Seat Legend</legend>
-<table class="center seating-chart">
-<col /><col /><col width="25" />
-<col /><col />
-<tr>
-<th class="vac2"><input type="checkbox" /></th><td>&nbsp; Available</td><td></td>
-<th class="occ"><input type="checkbox" /></th><td>&nbsp; Taken</td>
-</tr>
-</table>
-</fieldset>
+// <form action="#" onsubmit="return chooseSeat(this);">
+// ', $seat_str);
 
-<h1>Seating Plan</h1>
-<p>You must login in order to choose your seat.</p>
-<p>You may hover over any seat with your cursor to see its status.</p>
-';
-}
+// } else {
+//     $src .= '
+// <fieldset class="faded-bg" style="float:right;margin-left:1em;width:380px;">
+// <legend>Seat Legend</legend>
+// <table class="center seating-chart">
+// <col /><col /><col width="25" />
+// <col /><col />
+// <tr>
+// <th class="vac2"><input type="checkbox" /></th><td>&nbsp; Available</td><td></td>
+// <th class="occ"><input type="checkbox" /></th><td>&nbsp; Taken</td>
+// </tr>
+// </table>
+// </fieldset>
 
-// Generate the chart
-// Note: The chart will be displayed differently if the user is not logged in.
-$src .= genSeatChart($res_seats);
+// <h1>Seating Plan</h1>
+// <p>You must login in order to choose your seat.</p>
+// <p>You may hover over any seat with your cursor to see its status.</p>
+// ';
+// }
 
-if (isSet($_p)) {
-    $src .= '
-</form>
+// // Generate the chart
+// // Note: The chart will be displayed differently if the user is not logged in.
+// $src .= genSeatChart($res_seats);
 
-<script type="text/javascript">
-    $(".seating-chart.real").find("input").click(function() {chooseSeat(this.form);});
-</script>
-';
-}
+// if (isSet($_p)) {
+//     $src .= '
+// </form>
+
+// <script type="text/javascript">
+//     $(".seating-chart.real").find("input").click(function() {chooseSeat(this.form);});
+// </script>
+// ';
+// }
 
 mp($src, 'Seating Plan');
 
