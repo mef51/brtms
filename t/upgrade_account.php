586154b72c99933939de643c74080b3c1643e74f
mohammed chamma <hms.mohammed@gmail.com>
2014-02-11 20:36:21 -0500

    Update the 'upgrade account' page for this year

--- t/upgrade_account.php
+++ t/upgrade_account.php
@@ -12,43 +12,38 @@ require_once dirname(__FILE__) . '/../l/view.inc.php';
 requireAdminSession();
 
 // The possible upgraded registration levels
+// The numbers are % by 10 to get the credits. This array should associate the other way
 $tickets = array(
-	101 => 'Early Bird Ticket - 1 Major Tournament (upgrade)',
-	103 => 'Early Bird Ticket - 2-3 Major Tournaments (upgrade)',
-	110 => 'Early Bird Ticket - 4+ Major Tournaments (upgrade)',
-	1 => 'Regular Ticket - 1 Major Tournament (upgrade)',
-	3 => 'Regular Ticket - 2-3 Major Tournaments (upgrade)',
-	10 => 'Regular Ticket - 4+ Major Tournaments (upgrade)',
-	220 => 'Volunteer Ticket - 4+ Major Tournaments (upgrade)',
+	1 => '1 Major Tournament (upgrade)',
+	2 => '2 Major Tournaments (upgrade)',
+	12 => 'Console-only 2 Major Tournaments (upgrade)',
+	4 => 'Volunteer Ticket (upgrade)',
 );
 
 // If the form was submitted, ...
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	
+
 	$pid = $_POST['pid'];
 	$tic = (int) $_POST['tic'];
-	
+
 	if ($tic == 0) {
 		mp('<p>No Change.</p>');
 	}
-	
+
 	// Calculate their early-bird status and credit level
-	$early = (int) ($tic / 100);
-	$credits = $tic % 100;
-	
-	var_export($early);
-	echo '/';
+	$credits = $tic % 10;
+
 	var_export($credits);
-	
+
 	// Make the changes in the DB
 	$res = $db->query($sql = sPrintF('UPDATE `players`
-	  SET `early`=%2$s, `credits`=%3$s, `ticket`=%4$s
+	  SET `credits`=%2$s, `ticket`=%3$s
 	  WHERE `pid`=%1$s
-	  ', s($pid), s($early), s($credits), s($tickets[$tic])));
+	  ', s($pid), s($credits), s($tickets[$tic])));
 	if (!$res) {
 		error($sql);
 	}
-	
+
 	$res = $db->query($sql = sPrintF('SELECT `dname`, `fname`, `lname`, `token`
 	  FROM `players`
 	  WHERE `pid`=%1$s
@@ -60,19 +55,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if (!$p) {
 		die('Could not find player.');
 	}
-	
+
 	// Display the results page
-	$src = sPrintF('<h1>Reset Password: %1$s (%2$s %3$s)</h1>
+	$src = sPrintF('<h1>Upgraded Account: %1$s (%2$s %3$s)</h1>
 	<p>Successful!</p>
 	', $p['dname'], $p['fname'], $p['lname']);
-	
+
 	mp($src);
-	
+
 } else {
-	
+
 	// Form was not submitted; display the form
 	$pid = $_GET['pid'];
-	
+
 	$res = $db->query($sql = sPrintF('SELECT `dname`, `fname`, `lname`, `token`
 	  FROM `players`
 	  WHERE `pid`=%1$s
@@ -84,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if (!$p) {
 		die('Could not find player.');
 	}
-	
+
 	// Display the upgrade request form
 	$src = sPrintF('<h1>Upgrade Account: %1$s (%2$s %3$s)</h1>
 	<form action="#" method="post">
@@ -92,12 +87,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <select name="tic">
 <option value="0">No Change</option>
 	', $p['dname'], $p['fname'], $p['lname'], $pid);
-	
+
 	foreach ($tickets as $val => $name) {
 		$src .= sPrintF('<option value="%1$s">%2$s</option>',
 		  $val, $name);
 	}
 	$src .= '</select><input type="submit" value="Change" /></form>';
-	
-	mp($src);	
+
+	mp($src);
 }
